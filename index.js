8f35eccd1eeece9c520cd63c635f99c9474c9deb
OMsai <97650182+omsai11@users.noreply.github.com>
2022-12-01 15:44:56 +0530

    Files uploaded

---
+++ index.js
@@ -0,0 +1,36 @@
+let value="india";
+play();
+let xyzval;
+function xyz(xyzval)
+{  
+   value=xyzval;
+   document.getElementById("api").innerHTML=" "
+   play();
+}
+function play(){
+let file = `https://inshorts.deta.dev/news?category=${value}`
+fetch(file)
+.then(x=>x.text())
+    .then(y1 =>{
+        console.log(JSON.parse(y1));
+        ((JSON.parse(y1)).data).forEach(element => {
+            document.getElementById("api").innerHTML += `
+            <div class="container">
+           
+            <div class="Image">
+               <img src="${element.imageUrl}  width = "300" height = "300"" alt="" srcset="">
+               <h4> ${element.title} </h4> 
+               </div> 
+
+            <div class="content">
+                <p>${element.content}</p></br>
+                <h5>${element.date}</h5>
+            </div>
+
+            </div>
+        ` 
+        });
+    });
+}
+
+
